a6bfd0006ee07dd89d0e71a638d728a4fb1d3434
hantaeha <45843679+hantaeha@users.noreply.github.com>
2026-08-18 17:54:25 +0900

    Update app_control.py

--- app_control.py
+++ app_control.py
@@ -24,7 +24,7 @@ if "conversation_id" not in st.session_state:
 
 st.title("💬 실험 B")
 st.caption("본 대화는 총 5회의 질의응답으로 진행됩니다.")
-st.caption("첫째로 건네는 대화는 반드시 실험 B와 동일해야 합니다.")
+st.caption("첫 번째로 시작하는 대화는 반드시 실험 A와 동일하게 시작해야 합니다.")
 
 for msg in st.session_state.messages:
     with st.chat_message(msg["role"]):

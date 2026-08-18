20609181ccee5523fb78ca71cd1520d9524c3bc0
hantaeha <45843679+hantaeha@users.noreply.github.com>
2026-08-18 17:56:54 +0900

    Update app_prototype.py

--- app_prototype.py
+++ app_prototype.py
@@ -25,7 +25,7 @@ if "conversation_id" not in st.session_state:
 
 st.title("💬 실험 A ")
 st.caption("본 실험은 총 5회의 대화로 진행됩니다.")
-st.caption("첫 번째로 시작하는 대화는 반드시 실험 B와 동일하게 시작해야 합니다.")
+st.caption("첫 번째로 시작하는 대화는 반드시 실험 B와 동일해야 합니다.")
 
 # 지난 대화 출력
 for msg in st.session_state.messages:

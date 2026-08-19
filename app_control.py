084edf2425e771f5ef613aeb7593745db89c27ec
hantaeha <45843679+hantaeha@users.noreply.github.com>
2026-08-19 11:41:02 +0900

    Update app_control.py

--- app_control.py
+++ app_control.py
@@ -40,19 +40,6 @@ hide_footer_style = """
 
 st.markdown(hide_footer_style, unsafe_allow_html=True)
 
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
 # Streamlit Secrets에서 대조군 전용 API Key 로드
 DIFY_API_KEY = st.secrets["DIFY_API_KEY_CONTROL"]
 DIFY_API_URL = "https://api.dify.ai/v1/chat-messages"
